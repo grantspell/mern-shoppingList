e563ff1abf1bb653bd9c094427bf2baddac2eae1
grantspell <spellgrant@gmail.com>
2018-07-25 18:44:16 -0400

    UPDATED : with `.env` and MongoDB is up and running!

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Express Static Middleware
+// app.use(express.static(`${__dirname}/client/build`))
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+// Server Config
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
