ff81c36fc41e18b4322f461ad54c7446f92bd94d
grantspell <spellgrant@gmail.com>
2018-07-25 22:29:27 -0400

    UPDATED : root `package.json` with `heroku-postbuild` script for deployment

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
 const app = express();
 
-// Express Static Middleware
-// app.use(express.static(`${__dirname}/client/build`))
 
 // BodyParser Middleware
 app.use(bodyParser.json());
@@ -16,11 +15,21 @@ app.use(bodyParser.json());
 // DB Config
 mongoose.Promise = global.Promise
 mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err));
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
 
 // Use Routes
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // Server Config
 const port = process.env.PORT || 5000;
