1b11380a99bbf2d12f587791b91bb81cbf7b2aa5
grantspell <spellgrant@gmail.com>
2018-07-25 21:10:37 -0400

    ADDED : `AddItem` Modal + Redux methods/prop values[?] for adding an item

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+
+// REDUX STORE
+import { Provider } from 'react-redux';
+import store from './store';
 
 // COMPONENTS
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal';
 
 // STYLING
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,10 +17,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar />
-        <ShoppingList />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
     );
   }
 }
---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,29 @@
+import uuid from 'uuid';
+
+// ACTION TYPES FOR ITEMS
+import { GET_ITEMS, ADD_ITEMS, DELETE_ITEM } from '../actions/types';
+
+const initialState = {
+    items: [
+        { id: uuid(), name: 'Eggs' },
+        { id: uuid(), name: 'Milk' },
+        { id: uuid(), name: 'Steak' },
+        { id: uuid(), name: 'Candy' }
+    ]
+}
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case GET_ITEMS:
+            return {
+                ...state
+            };
+        case DELETE_ITEM:
+            return {
+                ...state,
+                items: state.items.filter(item => item.id !== action.payload)
+            };
+        default:
+            return state;
+    }
+}
