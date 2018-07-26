afe39d89fdae951d8c5ba8c38973ff77ba7743a5
grantspell <spellgrant@gmail.com>
2018-07-25 21:24:06 -0400

    UPDATED: ReduxStore to `ADD_ITEM` from UI `ItemModal`

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,16 +1,12 @@
 import uuid from 'uuid';
 
 // ACTION TYPES FOR ITEMS
-import { GET_ITEMS, ADD_ITEMS, DELETE_ITEM } from '../actions/types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
 
 const initialState = {
-    items: [
-        { id: uuid(), name: 'Eggs' },
-        { id: uuid(), name: 'Milk' },
-        { id: uuid(), name: 'Steak' },
-        { id: uuid(), name: 'Candy' }
-    ]
-}
+    items: [],
+    loading: false
+};
 
 export default function (state = initialState, action) {
     switch (action.type) {
@@ -22,6 +18,11 @@ export default function (state = initialState, action) {
             return {
                 ...state,
                 items: state.items.filter(item => item.id !== action.payload)
+            };
+        case ADD_ITEM:
+            return {
+                ...state,
+                items: [action.payload, ...state.items]
             };
         default:
             return state;
