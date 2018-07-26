41d783d58330157719892f74469ae3280ca26b2e
grantspell <spellgrant@gmail.com>
2018-07-25 21:43:17 -0400

    UPDATED : Redux Store `DELETE_ITEM`, application now properly Creates, Reads and Deletes items from backend Mongo Database

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,7 +1,7 @@
 import uuid from 'uuid';
 
 // ACTION TYPES FOR ITEMS
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
 
 const initialState = {
     items: [],
@@ -12,17 +12,24 @@ export default function (state = initialState, action) {
     switch (action.type) {
         case GET_ITEMS:
             return {
-                ...state
+                ...state,
+                items: action.payload,
+                loading: false
             };
         case DELETE_ITEM:
             return {
                 ...state,
-                items: state.items.filter(item => item.id !== action.payload)
+                items: state.items.filter(item => item._id !== action.payload)
             };
         case ADD_ITEM:
             return {
                 ...state,
                 items: [action.payload, ...state.items]
+            };
+        case ITEMS_LOADING:
+            return {
+                ...state,
+                loading: true
             };
         default:
             return state;
